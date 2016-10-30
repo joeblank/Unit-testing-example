4b2d6e530bcb39a24cb5710edb5131deeb23e8af
Joseph Blank <josephblank@Josephs-MacBook-Pro.local>
2016-10-29 21:54:18 -0600

    added unit test for post endpoint

--- server/test/productsCtrl.js
+++ server/test/productsCtrl.js
@@ -6,9 +6,10 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 var fakeProduct = {
-  Name: 'Fake Racquet',
-  Price: 950
+  name: 'Fake Racquet',
+  price: 950
 };
+
 
 describe('productsCtrl', function() {
 
@@ -45,10 +46,25 @@ describe('productsCtrl', function() {
 
     chai.request(server)
     .post('/addProduct')
-    .send(fakeProduct)
-
-
+    .send({name: fakeProduct.name, price: fakeProduct.price})
+    .end(function(err, res) {
+      console.log('this is what is sent: ' + fakeProduct);
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.ok;
+      expect(res.body).to.equal('created!');
+      console.log(res.body);
+      console.log(fakeProduct.Price);
+      done();
+    })
   })
+
+  // it('should delete fake racquet', function(done) {
+  //   chai.request(server)
+  //   .delete('/delete:id')
+  //
+  //
+  //
+  // })
 
 
 
